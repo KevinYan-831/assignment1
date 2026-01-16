f080d8576d01a5aa88f81022e81b3bc56a2e3681
Jixin (Kevin) Yan <JixinYan2029@u.northwestern.edu>
2026-01-15 19:22:46 -0600

    finish

--- asn0_kalt.py
+++ asn0_kalt.py
@@ -121,267 +121,18 @@ def set_all_default():
     time.sleep(1)
 
 
-#Front pair leg helper functions
-def front_pair_swing_forward(duration, amount):
-    board.bus_servo_set_position(duration, [[RF_INNER_ID, RF_INNER_DEFAULT - amount], [LF_INNER_ID, LF_INNER_DEFAULT + amount]])
-
-
-def front_pair_swing_backward(duration, amount):
-    board.bus_servo_set_position(duration, [[RF_INNER_ID, RF_INNER_DEFAULT + amount],  [LF_INNER_ID, LF_INNER_DEFAULT - amount]])
-
-
-def front_pair_swing_default(duration):
-    board.bus_servo_set_position(duration, [[RF_INNER_ID, RF_INNER_DEFAULT],[LF_INNER_ID, LF_INNER_DEFAULT]])
-
-
-def front_pair_push_down(duration, amount):
-    board.bus_servo_set_position(duration, [[RF_MIDDLE_ID, RF_MIDDLE_DEFAULT + amount], [LF_MIDDLE_ID, LF_MIDDLE_DEFAULT - amount]])
-
-
-def front_pair_lift_leg(duration, amount):
-    board.bus_servo_set_position(duration, [[RF_MIDDLE_ID, RF_MIDDLE_DEFAULT - amount], [LF_MIDDLE_ID, LF_MIDDLE_DEFAULT + amount]])
-
-
-def front_pair_middle_default(duration):
-    board.bus_servo_set_position(duration, [[RF_MIDDLE_ID, RF_MIDDLE_DEFAULT],[LF_MIDDLE_ID, LF_MIDDLE_DEFAULT]])
-
-
-def front_pair_extend_outward(duration, amount):
-    board.bus_servo_set_position(duration, [[RF_OUTER_ID, RF_OUTER_DEFAULT + amount],  [LF_OUTER_ID, LF_OUTER_DEFAULT - amount]])
-
-
-def front_pair_retract_inward(duration, amount):
-    board.bus_servo_set_position(duration, [[RF_OUTER_ID, RF_OUTER_DEFAULT - amount],  [LF_OUTER_ID, LF_OUTER_DEFAULT + amount]])
-
-
-def front_pair_outer_default(duration):
-    board.bus_servo_set_position(duration, [[RF_OUTER_ID, RF_OUTER_DEFAULT],[LF_OUTER_ID, LF_OUTER_DEFAULT]])
-
-# Middle pair leg helper functions
-def middle_pair_swing_forward(duration, amount):
-    board.bus_servo_set_position(duration, [[RM_INNER_ID, RM_INNER_DEFAULT - amount],  [LM_INNER_ID, LM_INNER_DEFAULT + amount]])
-
-
-def middle_pair_swing_backward(duration, amount):
-    board.bus_servo_set_position(duration, [[RM_INNER_ID, RM_INNER_DEFAULT + amount], [LM_INNER_ID, LM_INNER_DEFAULT - amount]])
-
-
-def middle_pair_swing_default(duration):
-    board.bus_servo_set_position(duration, [[RM_INNER_ID, RM_INNER_DEFAULT],[LM_INNER_ID, LM_INNER_DEFAULT]])
-
-
-def middle_pair_push_down(duration, amount):
-    board.bus_servo_set_position(duration, [[RM_MIDDLE_ID, RM_MIDDLE_DEFAULT + amount], [LM_MIDDLE_ID, LM_MIDDLE_DEFAULT - amount]])
-
-
-def middle_pair_lift_leg(duration, amount):
-    board.bus_servo_set_position(duration, [[RM_MIDDLE_ID, RM_MIDDLE_DEFAULT - amount], [LM_MIDDLE_ID, LM_MIDDLE_DEFAULT + amount]])
-
-
-def middle_pair_middle_default(duration):
-    board.bus_servo_set_position(duration, [[RM_MIDDLE_ID, RM_MIDDLE_DEFAULT],[LM_MIDDLE_ID, LM_MIDDLE_DEFAULT]])
-
-
-def middle_pair_extend_outward(duration, amount):
-    board.bus_servo_set_position(duration, [[RM_OUTER_ID, RM_OUTER_DEFAULT + amount], [LM_OUTER_ID, LM_OUTER_DEFAULT - amount]])
-
-
-def middle_pair_retract_inward(duration, amount):
-    board.bus_servo_set_position(duration, [[RM_OUTER_ID, RM_OUTER_DEFAULT - amount],  [LM_OUTER_ID, LM_OUTER_DEFAULT + amount]])
-
-
-def middle_pair_outer_default(duration):
-    board.bus_servo_set_position(duration, [[RM_OUTER_ID, RM_OUTER_DEFAULT],[LM_OUTER_ID, LM_OUTER_DEFAULT]])
-
 #Back pair leg helper functions
-def back_pair_swing_forward(duration, amount):
-    board.bus_servo_set_position(duration, [[RB_INNER_ID, RB_INNER_DEFAULT - amount],  [LB_INNER_ID, LB_INNER_DEFAULT + amount]])
-
-
 def back_pair_swing_backward(duration, amount):
     board.bus_servo_set_position(duration, [[RB_INNER_ID, RB_INNER_DEFAULT + amount], [LB_INNER_ID, LB_INNER_DEFAULT - amount]])
-
-
-def back_pair_swing_default(duration):
-    board.bus_servo_set_position(duration, [[RB_INNER_ID, RB_INNER_DEFAULT],[LB_INNER_ID, LB_INNER_DEFAULT]])
-
 
 def back_pair_push_down(duration, amount):
     board.bus_servo_set_position(duration, [[RB_MIDDLE_ID, RB_MIDDLE_DEFAULT + amount],  [LB_MIDDLE_ID, LB_MIDDLE_DEFAULT - amount]])
 
-
-def back_pair_lift_leg(duration, amount):
-    board.bus_servo_set_position(duration, [[RB_MIDDLE_ID, RB_MIDDLE_DEFAULT - amount], [LB_MIDDLE_ID, LB_MIDDLE_DEFAULT + amount]])
-
-
-def back_pair_middle_default(duration):
-    board.bus_servo_set_position(duration, [[RB_MIDDLE_ID, RB_MIDDLE_DEFAULT],[LB_MIDDLE_ID, LB_MIDDLE_DEFAULT]])
-
-
 def back_pair_extend_outward(duration, amount):
     board.bus_servo_set_position(duration, [[RB_OUTER_ID, RB_OUTER_DEFAULT + amount],  [LB_OUTER_ID, LB_OUTER_DEFAULT - amount]])
 
-
-def back_pair_retract_inward(duration, amount):
-    board.bus_servo_set_position(duration, [[RB_OUTER_ID, RB_OUTER_DEFAULT - amount],  [LB_OUTER_ID, LB_OUTER_DEFAULT + amount]])
-
-
 def back_pair_outer_default(duration):
     board.bus_servo_set_position(duration, [[RB_OUTER_ID, RB_OUTER_DEFAULT],[LB_OUTER_ID, LB_OUTER_DEFAULT]])
-
-#last 8.5 seconds
-def ripple_slow():
-    duration = 1.0
-    swing = 200
-    push = 200
-    crawl = 200
-    pause = 0.5
-    
-    # PHASE 1: Front and middle pair of legs swing forward
-    front_pair_swing_forward(duration, swing)
-    
-    middle_pair_lift_leg(pause, 50)  # Slight lift for stability
-    time.sleep(pause)
-    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
-
-    middle_pair_swing_forward(duration, swing)
-    time.sleep(pause)
-    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
-
-    middle_pair_middle_default(pause)
-    time.sleep(pause)
-    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
-
-    back_pair_swing_backward(duration, swing)
-    time.sleep(pause)
-    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
-
-    # PHASE 2: Front pair crawling
-    front_pair_push_down(duration, push)
-    time.sleep(pause)
-    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
-
-    front_pair_swing_default(duration)
-    time.sleep(pause)
-    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
-
-    front_pair_middle_default(duration)
-    time.sleep(pause)
-    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
-
-    # PHASE 3: Middle pair crawling
-    middle_pair_push_down(duration, push)
-    time.sleep(pause)
-    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
-
-    middle_pair_swing_default(duration)
-    time.sleep(pause)
-    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
-
-    middle_pair_middle_default(duration)
-    time.sleep(pause)
-    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
-
-    # PHASE 4: Back pair push forward and crawl
-    back_pair_push_down(duration, push)
-    time.sleep(pause)
-    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
-
-    back_pair_extend_outward(duration, crawl)
-    time.sleep(pause)
-    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
-
-    back_pair_middle_default(duration)
-    time.sleep(pause)
-    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
-
-    back_pair_outer_default(duration)
-    time.sleep(pause)
-    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
-    
-    back_pair_swing_default(duration)
-    time.sleep(pause)
-    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
-
-#lasts 5.5 seconds
-def ripple_fast():
-    duration = 0.6
-    swing = 200
-    push = 250
-    crawl = 250
-    pause = 0.3
-    
-    #  Front and middle pair of legs swing forward
-    front_pair_swing_forward(duration, swing)
-    
-    middle_pair_lift_leg(pause, 50)  # Slight lift for stability
-    time.sleep(pause)
-    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
-    middle_pair_swing_forward(duration, swing)
-    time.sleep(pause)
-    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
-
-    middle_pair_middle_default(pause)
-    time.sleep(pause)
-    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
-    
-    back_pair_swing_backward(duration, swing)
-    time.sleep(pause)
-    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
-
-    # PHASE 2: Front pair crawling
-    front_pair_push_down(duration, push)
-    time.sleep(pause)
-    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
-
-    front_pair_swing_default(duration)
-    time.sleep(pause)
-    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
-
-    front_pair_middle_default(duration)
-    time.sleep(pause)
-    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
-
-    # PHASE 3: Middle pair crawling
-    middle_pair_push_down(duration, push)
-    time.sleep(pause)
-    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
-
-    middle_pair_swing_default(duration)
-    time.sleep(pause)
-    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
-
-    middle_pair_middle_default(duration)
-    time.sleep(pause)
-    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
-
-    # PHASE 4: Back pair push forward and crawl
-    back_pair_push_down(duration, push)
-    time.sleep(pause)
-    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
-
-    back_pair_extend_outward(duration, crawl)
-    time.sleep(pause)
-    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
-
-    back_pair_middle_default(duration)
-    time.sleep(pause)
-    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
-
-    back_pair_outer_default(duration)
-    time.sleep(pause)
-    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
-
-    back_pair_swing_default(duration)
-    time.sleep(pause)
-    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
-
-
-
-
-
-    # Front right 
-
 
 #Discover the problem that if directly moving the pair of legs, the friction created by that will distort the robot, so lift and moving
 def front_pair_step_forward(duration, swing):
@@ -415,22 +166,6 @@ def middle_pair_step_forward(duration, swing):
         [LM_MIDDLE_ID, LM_MIDDLE_DEFAULT]
     ])
 
-
-    # Lift + swing backward
-    board.bus_servo_set_position(duration, [
-        [RB_MIDDLE_ID, RB_MIDDLE_DEFAULT - 50],
-        [LB_MIDDLE_ID, LB_MIDDLE_DEFAULT + 50],
-        [RB_INNER_ID, RB_INNER_DEFAULT + swing],  
-        [LB_INNER_ID, LB_INNER_DEFAULT - swing]    
-    ])
-    time.sleep(duration)
-    
-    # Lower 
-    board.bus_servo_set_position(0.5, [
-        [RB_MIDDLE_ID, RB_MIDDLE_DEFAULT],
-        [LB_MIDDLE_ID, LB_MIDDLE_DEFAULT]
-    ])
-
 def front_pair_push_and_swing_backward(duration, push, swing):
     board.bus_servo_set_position(duration, [
         [RF_MIDDLE_ID, RF_MIDDLE_DEFAULT + push],
@@ -447,14 +182,6 @@ def middle_pair_push_and_swing_backward(duration, push, swing):
         [LM_INNER_ID, LM_INNER_DEFAULT - swing]
     ])
 
-def back_pair_push_and_swing_backward(duration, push, swing):
-    board.bus_servo_set_position(duration, [
-        [RB_MIDDLE_ID, RB_MIDDLE_DEFAULT + push],   
-        [LB_MIDDLE_ID, LB_MIDDLE_DEFAULT - push],
-        [RB_INNER_ID, RB_INNER_DEFAULT + swing],   
-        [LB_INNER_ID, LB_INNER_DEFAULT - swing]
-    ])
-
 def back_pair_default(duration):
     board.bus_servo_set_position(duration, [
         [RB_MIDDLE_ID, RB_MIDDLE_DEFAULT],
@@ -462,43 +189,123 @@ def back_pair_default(duration):
         [RB_INNER_ID, RB_INNER_DEFAULT],
         [LB_INNER_ID, LB_INNER_DEFAULT]
     ])
+def front_pair_default(duration):
+    board.bus_servo_set_position(duration, [
+        [RF_MIDDLE_ID, RF_MIDDLE_DEFAULT],
+        [LF_MIDDLE_ID, LF_MIDDLE_DEFAULT],
+        [RF_INNER_ID, RF_INNER_DEFAULT],
+        [LF_INNER_ID, LF_INNER_DEFAULT]
+    ])
+def middle_pair_default(duration):
+    board.bus_servo_set_position(duration, [
+        [RM_MIDDLE_ID, RM_MIDDLE_DEFAULT],
+        [LM_MIDDLE_ID, LM_MIDDLE_DEFAULT],
+        [RM_INNER_ID, RM_INNER_DEFAULT],
+        [LM_INNER_ID, LM_INNER_DEFAULT]
+    ])
 
-def ripple_slow2():
+def ripple_slow():
     duration = 1.0
-    pause=0.5
     swing = 200
     push = 200
-
-    #Phase 1: Front and middle pair step forward, back pair step backward
-    front_pair_step_forward(duration, swing)
+    crawl = 200
+    pause = 0.5
+    
+    # Front and middle pair of legs swing forward, and back leg swing backward
+    front_pair_step_forward(duration,swing)
     time.sleep(pause)
     print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
 
-    middle_pair_step_forward(duration, swing)
+    middle_pair_step_forward(duration,swing)
     time.sleep(pause)
     print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
 
-    #Phase 2: Front push and swing backward
+    back_pair_swing_backward(duration, swing)
+    time.sleep(pause)
+    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
+
+    # Front pair push and move backward
     front_pair_push_and_swing_backward(duration, push, swing)
     time.sleep(pause)
     print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
+    front_pair_default(duration)
+    time.sleep(pause)
+    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
 
-    #Phase 3: Middle push and swing backward
+    # Middle pair push and move backward
     middle_pair_push_and_swing_backward(duration, push, swing)
     time.sleep(pause)
     print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
 
-    #Phase 4: Back push and swing backward
-    back_pair_push_and_swing_backward(duration, push, swing)
+    middle_pair_default(duration)
     time.sleep(pause)
+    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
+
+    # Back pair push forward and crawl
+    back_pair_push_down(duration, push)
+
+    back_pair_extend_outward(duration, crawl)
+    time.sleep(pause)
+    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
+
     back_pair_default(duration)
+
+    back_pair_outer_default(duration)
+    time.sleep(pause)
+    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
+    
+
+
+def ripple_fast():
+    duration = 0.6
+    swing = 200
+    push = 250
+    crawl = 250
+    pause = 0.3
+    
+    # Front and middle pair of legs swing forward, and back leg swing backward
+    front_pair_step_forward(duration,swing)
+    time.sleep(pause)
+    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
+
+    middle_pair_step_forward(duration,swing)
+    time.sleep(pause)
     print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
 
 
+    back_pair_swing_backward(duration, swing)
+    time.sleep(pause)
+    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
 
+    # Front pair push and move backward
+    front_pair_push_and_swing_backward(duration, push, swing)
+    time.sleep(pause)
+    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
+    front_pair_default(duration)
+    time.sleep(pause)
+    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
 
+    # Middle pair push and move backward
+    middle_pair_push_and_swing_backward(duration, push, swing)
+    time.sleep(pause)
+    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
 
+    middle_pair_default(duration)
+    time.sleep(pause)
+    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
 
+    # Back pair push forward and crawl
+    back_pair_push_down(duration, push)
+
+    back_pair_extend_outward(duration, crawl)
+    time.sleep(pause)
+    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
+
+    back_pair_default(duration)
+
+    back_pair_outer_default(duration)
+    time.sleep(pause)
+    print(f"[{time.time() - start_time}s] Sonar: {s.getDistance()} mm")
 
 
 
@@ -513,10 +320,17 @@ if __name__ == '__main__':
 
     while True:
 
-        ripple_slow()
-        time.sleep(1)
-        # ripple_fast()
-        # time.sleep(1)
+        ripple_fast()
+
+        
+
+
+
+
+
+
+
+
 
 
         
